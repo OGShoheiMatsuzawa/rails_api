89d1c36bb7f68aee47fc26ebcbd6c007a933bc5e
OGShoheiMatsuzawa <143977209+OGShoheiMatsuzawa@users.noreply.github.com>
2023-09-21 04:55:09 +0000

    Fix signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'auth/registrations'
+    }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "posts#index"
   namespace 'api' do
-    namespace 'v1' do
-      mount_devise_token_auth_for 'User', at: 'auth'
+    namespace 'v1', { format: 'json' } do
       resources :posts
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_072447) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_040725) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
